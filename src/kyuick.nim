0ce39a6d72a4436da185560586047fcb3742d7b9
ShujianDou <visionistprojects@gmail.com>
2023-04-30 03:44:06 -0400

    Save render information for labels, for efficiency

--- src/kyuick.nim
+++ src/kyuick.nim
@@ -22,31 +22,38 @@ var screenObjects: seq[kyuickObject] = @[]
 
 # Process mouse clicks and calculate object clicked.
 proc mousePress(e: MouseButtonEventPtr) =
-  echo e.button
   case e.button:
     of 1:
       for obj in screenObjects:
-        echo ("mouse($1, $2) objX($3, $4) objY($5, $6)" % [$e.x, $e.y, $obj.x, $(obj.x + obj.width), $obj.y, $(obj.y + obj.height)])
+        #echo ("mouse($1, $2) objX($3, $4) objY($5, $6)" % [$e.x, $e.y, $obj.x, $(obj.x + obj.width), $obj.y, $(obj.y + obj.height)])
         if e.x >= obj.x and e.x <= (obj.x + obj.width):
           if e.y >= obj.y and e.y <= (obj.y + obj.height):
             obj.leftClick()
+            return
     else:
       return
 
+var frameRate: Label
+var memLabel: Label
+
+# Load the TTF font 'liberation-sans.ttf' at fontsize 20.
+let fSize: cint = 24
+var font: FontPtr
+
 # Test proc, to be deleted.
 proc clicked(obj: kyuickObject) =
-  echo "Clicked object at ($1, $2)" % [$obj.x, $obj.y]
+  #echo "Clicked object at ($1, $2)" % [$obj.x, $obj.y]
+  screenObjects.add newLabel((cint)(obj.x + 20), (cint)obj.y, "pow!", [100, 100, 100, 255], font, fSize)
 
-var frameRate: Label
 proc testRendering*() =
-  # Load the TTF font 'liberation-sans.ttf' at fontsize 20.
-  let fSize: cint = 24
-  let font = ttf.openFont("liberation-sans.ttf", fSize)
+  font = ttf.openFont("liberation-sans.ttf", fSize)
   # Create our white label at (100,100) with our font.
   screenObjects.add newLabel(100, 100, "Lorem Ipsum Dollarunis",
     [255, 255, 255, 255], font, fSize)
   frameRate = newLabel(10, 10, "FPS: ", [25, 255, 100, 255], font, fSize)
+  memLabel = newLabel(10, 30, "Occ. Mem: ", [25, 255, 100, 255], font, fSize)
   screenObjects.add frameRate
+  screenObjects.add memLabel
   screenObjects.add newButton(100, 150, 250, 50, [25, 255, 100, 255], "Button",
     font, fSize, [0, 0, 0, 255])
   # Debug loop assigning onLeftClick for every object, (to be deleted)
@@ -62,10 +69,14 @@ proc startGameLoop*(name: string) =
   let window = sdl2.createWindow(name, WinXPos, WinYPos, WinWidth, WinHeight, flags = SDL_WINDOW_SHOWN)
   # Create our renderer with V-Sync
   let renderer = createRenderer(window = window, index = -1,
-    flags = Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+    flags = Renderer_Accelerated or Renderer_TargetTexture)
 
   testRendering()
-
+  # (stress test)
+  var i: int = 0
+  while i < 100000:
+    screenObjects.add newLabel(1000, 30, "Occ. Mem: ", [25, 255, 100, 255], font, fSize)
+    inc i
   var startCounter = getPerformanceCounter()
   var endCounter = getPerformanceCounter()
   # Start the infinite renderer.
@@ -89,6 +100,9 @@ proc startGameLoop*(name: string) =
     endCounter = getPerformanceCounter()
     frameRate.text = "FPS: " & $(int)(1 / ((endCounter - startCounter).float /
       getPerformanceFrequency().float))
+    memLabel.text = "Res. Mem: " & $(getTotalMem().float / 1000.0f) & " KB" & " | Used: " & $int((getOccupiedMem().float / 1000.0f)) & " KB"
+    #echo GC_getStatistics()
+    #echo $len(screenObjects)
 
 when isMainModule:
   startGameLoop("tester")
--- src/kyuick/components/label.nim
+++ src/kyuick/components/label.nim
@@ -10,24 +10,42 @@ import kyuickObject
 
 # A label object for displaying text at a line of text at a x,y coordinate.
 type Label* = ref object of kyuickObject
-  text*: string
+  text: string
   color*: array[4, int]
   font*: FontPtr
+  rect: Rect
+  texture: TexturePtr
+  renderSaved*: bool
   fontSize*: cint
 
 proc renderLabel*(renderer: RendererPtr, obj: kyuickObject) =
+  let label: Label = (Label)obj
+  if label.renderSaved:
+    renderer.copy label.texture, nil, addr label.rect   
+    return
   let
-    label: Label = (Label)obj
     surface = ttf.renderTextBlended(label.font, label.text, color(label.color[0],
       label.color[1], label.color[2], label.color[3]))
     texture = renderer.createTextureFromSurface(surface)
   surface.freeSurface()
-  defer: texture.destroy()
+  label.texture = texture
   var r = rect(label.x, label.y, label.width, label.height)
+  label.rect = r
   renderer.copy texture, nil, addr r
+  label.renderSaved = true
 
 proc newLabel*(x, y: cint, text: string, color: array[4, int], font: FontPtr, fontSize: cint): Label =
   var w, h: cint = 0
   discard ttf.sizeText(font, text, addr w, addr h)
   return Label(x: x, y: y, width: w,
-    height: h, text: text, color: color, font: font, render: renderLabel, fontSize: fontSize)
+    height: h, text: text, color: color, font: font, render: renderLabel, renderSaved: false, fontSize: fontSize)
+
+proc text*(this: Label): string = this.text
+proc `text=`*(this: var Label, text: string) = 
+  this.text = text
+  var w, h: cint = 0
+  discard ttf.sizeText(this.font, text, addr w, addr h)
+  this.width = w
+  this.height = h
+  this.texture.destroy()
+  this.renderSaved = false
