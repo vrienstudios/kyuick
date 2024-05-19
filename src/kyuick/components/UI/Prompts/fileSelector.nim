5adf708564f4215cf9dbf2006136a124f9636c51
ShujianDou <visionistprojects@gmail.com>
2024-05-19 02:54:49 -0400

    fs work

---
+++ src/kyuick/components/UI/Prompts/fileSelector.nim
@@ -0,0 +1,55 @@
+import ../all
+import ../../kyuickObject
+import ../../Grids/verticalGrid
+import ../../Grids/horizontalGrid
+
+# Window meant for selecting files.
+type FileSelector* = ref object of KyuickObject
+  path*: string
+  tInput*: TextInput
+  btnSelect*: Button
+  onClick: proc(res: string)
+  #fileList*: VerticalGrid
+  #interactives*: HorizontalGrid
+  #[
+    fileName*: TextInput
+    cancel*: Button
+    open*: Button
+  ]#
+proc newFileItem(this: FileSelector, filePath: string): HorizontalGrid =
+  var fileInfo: HorizontalGrid =
+    uiGen HorizontalGrid:
+      height 100
+      width this.width
+  return
+#proc buildFileList(this: FileSelector) =
+#  for file in walkFiles(this.path):
+#    fileList.add newFileItem(file)
+proc defaultOnClick*(obj: KyuickObject, mouseEvent: MouseButtonEventPtr) =
+  let fs = FileSelector(obj)
+  fs.onClick(fs.tInput.textField.text)
+proc newFileSelector*(pstr: string, font: FontPtr, fontSize: cint, onClick: proc(res: string)): FileSelector =
+  var input = uiGen TextInput:
+    x 0
+    y 0
+    width 100
+    height 20
+    font font
+    fontSize fontSize
+  var btn = uiGen Button =
+    x 30
+    y 25
+    w 30
+    h 30
+    backgroundColor [0, 0, 0, 255]
+    text "OK"
+    font font
+    fontSize fontSize
+    foregroundColor [255, 255, 255, 255]
+  btn.onLeftClick = defaultOnClick
+  var fs = uiCon FileSelector:
+    path pstr
+    tInput input
+    btnSelect btn
+    passthrough true
+  return fs
